a6ce89653087bdd6ccd3d8f4016945b1ead82fa1
Smit Patel <smit.patel@nickelfox.com>
2022-07-18 14:10:08 +0530

    [feature](Validation): add validation to specific fields

--- bus_operator/models.py
+++ bus_operator/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from common.models import BaseModel
 from django.contrib.auth import get_user_model
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 User = get_user_model()
 
@@ -23,21 +24,12 @@ class BusOperatorProfile(BaseModel):
         blank=True,
     )
     office_address = models.TextField()
-    ratings = models.IntegerField(null=True, blank=True, default=None)
+    ratings = models.IntegerField(null=True, blank=True, default=None, validators=[
+            MaxValueValidator(10),
+            MinValueValidator(0)
+        ])
     approval_status = models.CharField(choices=APPROVAL_STATUS, max_length=20)
     rejection_comment = models.TextField(blank=True)
 
     def __str__(self) -> str:
         return "{} ({})".format(self.business_name, self.id)
-
-    def clean_ratings(self) -> int:
-        print("cleaned data:", self.cleaned_data)
-        ratings = self.cleaned_data.get("ratings",  False)
-        print(ratings)
-        return ratings
-    
-    def clean(self) -> dict:
-        print("cleaning")
-        cleaned_data = super().clean()
-        print("cleaned data:", cleaned_data)
-        return cleaned_data
