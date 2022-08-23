1c3c22b396cf4f1f26f70a20cfacea398e743206
Smit Patel <smit.patel@nickelfox.com>
2022-08-23 16:18:31 +0530

    [test](): Add management command to load large number of buses data. add locust file to run load testing on locust

---
+++ locustfile.py
@@ -0,0 +1,14 @@
+from locust import HttpUser, between, task
+import random
+
+class WebsiteUser(HttpUser):
+    wait_time = between(3, 5)
+    counter = 0
+    stops = ["Ahmedabad", "Udaypur", "Jodhpur", "Jaipur", "Delhi", "Kanpur"]
+    
+    @task
+    def search(self):
+        frm = random.choice(self.stops)
+        to = random.choice(self.stops)
+        self.client.get(f"http://localhost:8000/api/v1/buses/search?date=22-07-2022&from={frm}&to={to}")
+
